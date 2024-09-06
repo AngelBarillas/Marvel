13b7c5ac2f14f87695d34969cfa5b2a3e8e61624
Angel Barillas <a.e.barillas.ablan@gmail.com>
2024-09-06 13:49:01 -0400

    first commit

---
+++ charsearch.js
@@ -0,0 +1,32 @@
+const charNameValue = localStorage.getItem("charName");
+console.log(charNameValue);
+
+const fetchOneCharacter = async function (charNameValue) {
+  try {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/characters?name=${charNameValue}&limit=1&apikey=c5b21bcd83f9cb497db19d4a57fb1837`
+    );
+    const {
+      data: { results: character },
+    } = await response.json();
+    console.log(character);
+
+    const characterName = character[0].name;
+    const characterDesc = character[0].description;
+    const characterImg = `${character[0].thumbnail.path}/portrait_xlarge.jpg`;
+    document.getElementById("search-char-name").innerText = characterName;
+    if (characterDesc) {
+      document.getElementById("search-char-desc").innerText = characterDesc;
+    } else {
+      document.getElementById(
+        "search-char-desc"
+      ).innerText = `Sorry, we dont have a description for ${characterName} `;
+    }
+
+    document.getElementById("search-char-img").src = characterImg;
+  } catch (error) {
+    console.log(`there was an error with the request: ${error}`);
+  }
+};
+
+fetchOneCharacter(charNameValue);
---
+++ landing.js
@@ -0,0 +1,53 @@
+let currentOffset = 0;
+let searchName = "";
+
+const fetchCharacters = async function () {
+  try {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/characters?offset=${currentOffset}&apikey=c5b21bcd83f9cb497db19d4a57fb1837`
+    );
+    const data = await response.json();
+    console.log(data);
+    const characters = data.data.results;
+    console.log(characters);
+
+    const charactersNames = new Array();
+    const charactersImg = new Array();
+    const charactersDesc = new Array();
+
+    for (let i = 0; i < characters.length; i++) {
+      charactersNames.push(characters[i].name);
+      charactersImg.push(`${characters[i].thumbnail.path}/portrait_xlarge.jpg`);
+      charactersDesc.push(characters[i].description);
+
+      console.log(charactersNames[i]);
+      const ul = document.querySelector(".character-list");
+      const li = document.createElement("li");
+      const span = document.createElement("span");
+      const image = document.createElement("img");
+      ul.append(li);
+      li.setAttribute("id", `list-item${i}`);
+      li.setAttribute("class", "list-item");
+      li.append(image);
+      li.append(span);
+      span.setAttribute("id", `char-img-container${i}`);
+      span.setAttribute("class", `char-img-container`);
+      li.append(span);
+      span.setAttribute("id", `char-name-text${i}`);
+      span.innerText = `${charactersNames[i]}`;
+      image.setAttribute("id", `character-img${i}`);
+      image.setAttribute("src", `${charactersImg[i]}`);
+      image.setAttribute("alt", `image of ${charactersNames[i]}`);
+    }
+    currentOffset = currentOffset + data.data.count;
+  } catch (error) {
+    console.log(`there was an error with the request: ${error}`);
+  }
+};
+
+fetchCharacters();
+
+const saveSearchName = function () {
+  searchName = document.getElementById("search-input").value;
+  localStorage.setItem("charName", `${searchName}`);
+};
